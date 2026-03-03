c6cecd9374187cbf76c87a0bd0456bfc177524f1
Grzegorz Wilczyński <292416+gwilczynski@users.noreply.github.com>
2026-03-03 20:59:13 +0100

    feat: add new radio stations and associated assets

--- src/data/stations.js
+++ src/data/stations.js
@@ -1,5 +1,9 @@
 import r357Logo from "../assets/357.webp";
 import nsLogo from "../assets/ns.webp";
+import kampusLogo from "../assets/kampus.webp";
+import luzLogo from "../assets/luz.webp";
+import jazzLogo from "../assets/jazz.jpg";
+import rockLogo from "../assets/rock.webp";
 
 export const stations = [
   {
@@ -13,5 +17,29 @@ export const stations = [
     name: "Radio Nowy Świat",
     logoUrl: nsLogo,
     streamUrl: "https://stream.rcs.revma.com/ypqt40u0x1zuv",
+  },
+  {
+    id: "kampus",
+    name: "Radio Kampus",
+    logoUrl: kampusLogo,
+    streamUrl: "https://stream.radiokampus.fm/kampus",
+  },
+  {
+    id: "luz",
+    name: "Radio Luz",
+    logoUrl: luzLogo,
+    streamUrl: "https://stream.radioluz.pl:8443/luz_hifi.mp3",
+  },
+  {
+    id: "jazz",
+    name: "Radio JazzKultura",
+    logoUrl: jazzLogo,
+    streamUrl: "https://stream.radio.co/s1136b59f3/listen",
+  },
+  {
+    id: "rock",
+    name: "Radio RockSerwis",
+    logoUrl: rockLogo,
+    streamUrl: "https://stream9.nadaje.com:8003/live",
   },
 ];
